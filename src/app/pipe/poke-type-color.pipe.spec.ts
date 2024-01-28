aade83ddc399b51de6578e3cb86148afab60e771
jean519 <129189905+jean519@users.noreply.github.com>
2024-01-28 23:21:14 +0100

    ajout du pipe pour les types

---
+++ src/app/pipe/poke-type-color.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PokeTypeColorPipe } from './poke-type-color.pipe';
+
+describe('PokeTypeColorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PokeTypeColorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
