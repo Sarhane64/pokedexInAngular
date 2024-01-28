02e46eed8c1a985cdf60acc3ed7c32d7c2cbb590
jean519 <129189905+jean519@users.noreply.github.com>
2024-01-28 23:19:57 +0100

    affichage de la pokkelist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http/index.js';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokelistComponent } from './pokelist/pokelist.component';
 import { PokemonservService } from './pokemon-service.service.js';
 import { PokeDetailComponent } from './poke-detail/poke-detail.component';
+import { PokeTypeColorPipe } from './pipe/poke-type-color.pipe';
 
 @NgModule({
-  declarations: [AppComponent, PokelistComponent, PokeDetailComponent],
+  declarations: [
+    AppComponent,
+    PokelistComponent,
+    PokeDetailComponent,
+    PokeTypeColorPipe,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [PokemonservService],
   bootstrap: [AppComponent],
